8de7424b9c9cd71feee9f99c4a37e0a17b061131
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2023-10-27 21:08:36 -0500

    changed around html to work javascript user can add, pull, and display data from the database

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
+//import files to work with database - listing off the functions of each i will need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+//setting database location
 const appSettings = {
     databaseURL: "https://hall-of-champions-78f1d-default-rtdb.firebaseio.com/"
 }
 
+//setting up database
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const championsInDB = ref(database, "champions");
-
-let test = "test input";
-function pushtest() {
-    push(championsInDB, test)
-
-}
-pushtest()
-
 
 //user inputs
 const userCommentEl = document.querySelector("#user-comment-el");
@@ -28,6 +23,7 @@ const pulledToEl= document.querySelector("#pulled-to-el");
 const pulledCommentEl = document.querySelector("#pulled-comment-el");
 const pulledFromEl = document.querySelector("#pulled-from-el");
 const userLikesEl = document.querySelector("#pulled-likes-el");
+const consoleContainerEl = document.querySelector("#console-container-el")
 
 
 //buttons
@@ -35,4 +31,58 @@ const iconBtn = document.querySelector("#icon-btn");
 const SubmitBtn = document.querySelector("#Submit-btn");
 
 
+SubmitBtn.addEventListener("click", function() {
+    const userInputsArray = {}
+    userInputsArray["userTo"] = userToEl.value;
+    userInputsArray["userFrom"] = userFromEl.value;
+    userInputsArray["userComment"] = userCommentEl.value;
+    push(championsInDB, userInputsArray)
+})
+
+onValue(championsInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let storedUserData = Object.entries(snapshot.val())
+        console.log("snapshot value " + snapshot.val()) 
+        clearUserPosts()
+        for (let i = 0; i < storedUserData.length; i++) {
+            let currentItem = storedUserData[i];
+            let currentItemValue = currentItem[1]
+            let currentItemId = currentItem[0]
+            makeElement(currentItem)
+        }
+    } else {
+        consoleContainerEl.innerHTML = "no posts yet"
+    }
+}) 
+
+function makeElement(id) {
+    let userObject = id[1]
+    let makeTo = userObject["userTo"]
+    let makeFrom = userObject["userFrom"]
+    let makeComment = userObject["userComment"]
+
+    let newToEl = document.createElement("h3")
+    newToEl.textContent = makeTo;
+    let newFromEl = document.createElement("h3")
+    newFromEl.textContent = makeFrom;
+    let newCommentEl = document.createElement("p")
+    newCommentEl.textContent = makeComment;
+
+    const newElement = window.document.createElement('div');
+    newElement.classList.add('userPosts');
+    newElement.append(newToEl, newFromEl, newCommentEl)
+    window.document.querySelector("#console-container-el").appendChild(newElement)
+}
+
+
+/*function updateHTML(input) {
+    let itemId= input[0]
+    let itemValue= input[1]
+    let newToEl = document.createElement('li');
+    newToEl.textContent = itemValue
+} */
+
+function clearUserPosts() {
+    consoleContainerEl.innerHTML = "";
+}
 
